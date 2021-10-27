dd1e32d65b64c6d73365b131b3a413830512deed
inna.yatsyk <inna.yatsyk@globallogic.com>
2021-10-27 11:10:55 +0300

    "Create User" request validation

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,11 +1,38 @@
 
-import org.junit.Test;
-import org.hamcrest.io.FileMatchers;
-
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+import org.json.JSONObject;
+import static io.restassured.RestAssured.*;
 
 public class MainTest {
+
+    /**this test creates new user and asserts the success of
+      the creation by status code 201*/
+
     @Test
-    public void test(){
+    public void postTest(){
+
+     JSONObject request = new JSONObject();
+
+     request.put("id", "1199");
+     request.put("name", "VictorMalevsky5");
+     request.put("email", "victor5@gmail.com");
+     request.put("gender", "male");
+     request.put("status","active");
+
+     String token = "6e398124a1dd58b3e41e21ec5e41a269137a8771586f4fc839091bcc99606ed3";
+
+      given().
+             header("Content-Type", "application/json").
+             contentType(ContentType.JSON).
+             accept(ContentType.JSON).
+             auth().
+             oauth2(token).
+             body(request.toString()).
+             when().
+             post("https://gorest.co.in/public/v1/users").
+             then().
+             statusCode(201);
 
     }
 
