34fcaa765e244c87a3c98a69d5b1959252219d0b
inna.yatsyk <inna.yatsyk@globallogic.com>
2021-10-28 13:40:32 +0300

    Using class User for data generation and storage, using testNG for assertion.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,61 +1,47 @@
 
-import com.github.javafaker.Faker;
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
-import org.json.JSONObject;
-import org.testng.log4testng.Logger;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import java.util.Locale;
-
+import static org.testng.Assert.assertEquals;
+import static org.apache.http.HttpStatus.SC_CREATED;
+import org.json.*;
 import static io.restassured.RestAssured.*;
 
 public class MainTest {
-    private static Logger logger = Logger.getLogger(MainTest.class);
 
-    /**this test creates new user and asserts the success of
-      the creation by status code 201*/
+    /**
+     * This test creates new user and asserts the success of
+      the creation by status code 201 and equality of every field*/
 
     @Test
     public void postTest(){
-        /**creation of Faker object for
-         random test data generation*/
+        User userRequest= new User(); //the User object creation with generation of fake data
+        String token = "6e398124a1dd58b3e41e21ec5e41a269137a8771586f4fc839091bcc99606ed3";
 
-        Faker faker = new Faker(new Locale("en-GB"));
-        logger.info("Creation of Faker object for random test data generation");
-        String name = faker.name().fullName();
-        String email = faker.internet().emailAddress();
-        String gender = faker.random().nextBoolean()?"male":"female";
-        String status = "active";
+      String response;
+        response = given().
+                header("Content-Type", "application/json").
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                auth().
+                oauth2(token).
+                body(userRequest.toJString()).//the given String is in JSON format
+                when().
+                log().
+                all().
+                post("https://gorest.co.in/public/v1/users").
+                then().
+                assertThat().
+                statusCode(SC_CREATED).
+                extract().asString();
 
-     JSONObject request = new JSONObject();
+        JSONObject responseObj = new JSONObject(response);
+        User userResponse = new User(responseObj);//the User object creation with data from response
 
-     request.put("name", name);
-     request.put("email", email);
-     request.put("gender", gender);
-     request.put("status",status);
-    logger.info("Request user:/n"+ request.toString());
-     String token = "6e398124a1dd58b3e41e21ec5e41a269137a8771586f4fc839091bcc99606ed3";
-
-     String response=  given().
-             header("Content-Type", "application/json").
-             contentType(ContentType.JSON).
-             accept(ContentType.JSON).
-             auth().
-             oauth2(token).
-             body(request.toString()).
-             when().
-             post("https://gorest.co.in/public/v1/users").
-             then().
-             statusCode(201)
-            .extract().asString();
-    System.out.println(response);
-    assertThat(response.contains("\"name\":\""+name), is(true));
-    assertThat(response.contains("\"email\":\""+email), is(true));
-    assertThat(response.contains("\"gender\":\""+gender), is(true));
-    assertThat(response.contains("\"status\":\""+status), is(true));
-
+        //assertion of response using testNG
+        assertEquals(userResponse.getName(), userRequest.getName());
+        assertEquals(userResponse.getEmail(), userRequest.getEmail());
+        assertEquals(userResponse.getGender(), userRequest.getGender());
+        assertEquals(userResponse.getStatus(), userRequest.getStatus());
     }
 
 }
