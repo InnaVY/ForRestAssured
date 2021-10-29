0cd082e51c59ecc995b97396041a151626ad3d26
inna.yatsyk <inna.yatsyk@globallogic.com>
2021-10-29 16:16:17 +0300

    assertions after then()

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,47 +1,37 @@
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
-import static org.testng.Assert.assertEquals;
 import static org.apache.http.HttpStatus.SC_CREATED;
-import org.json.*;
 import static io.restassured.RestAssured.*;
 
 public class MainTest {
+    private String token = "6e398124a1dd58b3e41e21ec5e41a269137a8771586f4fc839091bcc99606ed3";
 
     /**
      * This test creates new user and asserts the success of
       the creation by status code 201 and equality of every field*/
 
     @Test
-    public void postTest(){
-        User userRequest= new User(); //the User object creation with generation of fake data
-        String token = "6e398124a1dd58b3e41e21ec5e41a269137a8771586f4fc839091bcc99606ed3";
+    public void postRestAssured(){
+        RestAssured.baseURI="https://gorest.co.in/public/v1/users";
+        User userRequest= new User();
+       given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer "+token)
+                .body(userRequest.toJString())
+                .when()
+                .post(baseURI)
+                       .then()
+                       .statusCode(SC_CREATED)
+                       .assertThat().body("data.name", Matchers.equalTo(userRequest.getName()))
+                       .assertThat().body("data.email", Matchers.equalTo(userRequest.getEmail()))
+                       .assertThat().body("data.gender", Matchers.equalTo(userRequest.getGender()))
+                       .assertThat().body("data.status", Matchers.equalTo(userRequest.getStatus()))
+                       .log()
+                       .all();
 
-      String response;
-        response = given().
-                header("Content-Type", "application/json").
-                contentType(ContentType.JSON).
-                accept(ContentType.JSON).
-                auth().
-                oauth2(token).
-                body(userRequest.toJString()).//the given String is in JSON format
-                when().
-                log().
-                all().
-                post("https://gorest.co.in/public/v1/users").
-                then().
-                assertThat().
-                statusCode(SC_CREATED).
-                extract().asString();
 
-        JSONObject responseObj = new JSONObject(response);
-        User userResponse = new User(responseObj);//the User object creation with data from response
-
-        //assertion of response using testNG
-        assertEquals(userResponse.getName(), userRequest.getName());
-        assertEquals(userResponse.getEmail(), userRequest.getEmail());
-        assertEquals(userResponse.getGender(), userRequest.getGender());
-        assertEquals(userResponse.getStatus(), userRequest.getStatus());
     }
-
 }
